ac37537fc29ddde43199548a8c42be8c4333d807
Alexander Slavutin <alexander.slavutin@xperience.ai>
2021-01-16 12:20:47 +0300

    start commit

---
+++ utils/article.py
@@ -0,0 +1,7 @@
+class Article:
+    def __init__(self, id, title, category, tags, text):
+        self.id = id
+        self.title = title
+        self.category = category
+        self.tags = tags
+        self.text = text
